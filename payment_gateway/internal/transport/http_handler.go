591356f4e7778eeda8a59d845905f55e4f26016d
Val <regval@pm.me>
2023-03-23 21:12:39 +0800

    del unused models

--- payment_gateway/internal/transport/http_handler.go
+++ payment_gateway/internal/transport/http_handler.go
@@ -26,26 +26,6 @@ func NewHandler(service payment.Service, logger *log.Logger) *Handler {
 	}
 }
 
-type PaymentRequest struct {
-	Card struct {
-		Number      string `json:"number"`
-		ExpiryMonth string `json:"expiry_month"`
-		ExpiryYear  string `json:"expiry_year"`
-		CVV         string `json:"cvv"`
-		HolderName  string `json:"holder_name"`
-	} `json:"card"`
-	Amount   int64  `json:"amount"`
-	Currency string `json:"currency"`
-}
-
-type PaymentResponse struct {
-	ID               string
-	TrackingID       string
-	MaskedCardNumber string
-	Status           string `json:"status"`
-	Code             string `json:"code"`
-}
-
 // Payment
 // @Summary
 // @Description  Create Payment
